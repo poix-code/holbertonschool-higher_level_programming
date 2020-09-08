51162b006cb6722fe6fab38d79d6b52b4b60281f
poix-code <1996@holbertonschool.com>
2020-09-08 23:59:53 +0000

    Function tested in python tutor

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_cpy = my_list[:]
+    if idx < 0 or idx >= len(my_list):
+        return list_cpy
+    list_cpy[idx] = element
+    return list_cpy
