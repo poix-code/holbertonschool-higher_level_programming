b707e46f93dd1267a319675df14059049fb7ae4f
poix-code <1996@holbertonschool.com>
2020-09-11 03:53:51 +0000

    Function tested in python tutor, resource (https://thepythonguru.com/python-builtin-functions/max/)

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        note = max(a_dictionary)
+        note = max(a_dictionary, key=a_dictionary.get)
         return note
     return None
