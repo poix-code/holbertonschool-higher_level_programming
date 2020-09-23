81e4df350d7b1d26ad2f908fb963fc17c6270857
poix-code <1996@holbertonschool.com>
2020-09-23 01:23:31 +0000

    Tested before uploaded

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,8 @@
 
 
 class Square:
-    """Define the object/instance                                              
+    """Define the object/instance
+
     Attributes: size(int)"""
 
     def __init__(self, size=0):
@@ -15,4 +16,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        
