bba7166e3ba162d9f2577ef634638db081f8f0cc
poix-code <1996@holbertonschool.com>
2020-09-23 01:21:48 +0000

    Tested before uploaded

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """Define the object/instance                                              
+    Attributes: size(int)"""
+
+    def __init__(self, size=0):
+        """Define attributes of the instance
+           and raise on a safe mode.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
