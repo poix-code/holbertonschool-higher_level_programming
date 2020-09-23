fc3931d99fc80c2cb8634fd24281f9f5844abf44
poix-code <1996@holbertonschool.com>
2020-09-23 01:45:08 +0000

    Tested in python tutor

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,20 +4,18 @@
 
 class Square:
     """Define the object/instance
-
     Attributes: size(int)"""
 
     def __init__(self, size=0):
         """Define attributes of the instance
-        and raise on a safe mode.
-        """
+        and raise on a safe mode."""
 
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         """Computes the area of a square"""
 
