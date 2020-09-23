01f5b8a615c3264b43ff82bb6ebb26de545ea759
poix-code <1996@holbertonschool.com>
2020-09-23 00:04:28 +0000

    Tested in python3

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Define a square class
-"""
+"""Define a square class"""
 
 class Square:
     """Define the object/instance
-    Attributes: size(Private)
-    """
+    Attributes: size(Private)"""
+
     def __init__(self, size):
-        """Define attributes of the instance
-        """
+        """Define attributes of the instance"""
+
         self.__size = size
