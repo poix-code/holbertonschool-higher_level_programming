f87c593028a8bd569bd5e7e3cd627c2ae4a2dedb
poix-code <1996@holbertonschool.com>
2020-09-27 22:37:29 +0000

    Changed the format (pep8)

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,10 +6,10 @@ Describes a function to adding two integers
 
 def add_integer(a, b=98):
     """Take the arguments to add.
-    
-    -a: One of the numbers, default not defined.
-    -b: The other number, default 98.
-    
+
+    a: One of the numbers, default not defined.
+    b: The other number, default 98.
+
     Return: int(a + b)
     """
     if type(a) not in [int, float]:
