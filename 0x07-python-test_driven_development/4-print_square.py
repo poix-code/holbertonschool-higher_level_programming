8cda525aacc8e151b46bc6c8e88f8b02883ad841
poix-code <1996@holbertonschool.com>
2020-09-28 02:56:38 +0000

    Tested with doctest

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function to print a square
+"""
+
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        print()
+    else:
+        for i in range(size):
+            print('#' * size)
