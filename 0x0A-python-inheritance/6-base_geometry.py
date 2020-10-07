72c54e6318da1b480da74794d5d959828b515909
poix-code <1996@holbertonschool.com>
2020-10-07 02:49:15 +0000

    Tested in python tutor

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """added the method 'area'"""
+    def area(self):
+        raise TypeError("area() is not implemented")
