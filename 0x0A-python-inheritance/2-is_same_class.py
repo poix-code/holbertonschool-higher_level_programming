75e272e823140bb1a1d8d350d344cb7494021978
poix-code <1996@holbertonschool.com>
2020-10-07 02:00:16 +0000

    change the conditionals of the return

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ Check if the object is an instance of a specific class.
 
 def is_same_class(obj, a_class):
     """Using the method 'isinstance()' to check"""
-    return isinstance(type(obj), a_class)
+    return type(obj) is a_class
