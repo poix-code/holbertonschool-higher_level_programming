cb6432f0797f9242519dbaa3c4e0b0ceedc9d8d8
poix-code <1996@holbertonschool.com>
2020-10-14 01:18:38 +0000

    Resource the manual

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,14 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON rep. in a file"""
+        l = []
+        fd = "{}.json".format(cls.__name__)
+        if list_objs:
+            for instance in list_objs:
+                ls.append(instance.to_dictionary())
+        with open(filename, mode="w", encoding="utf-8") as f:
+            f.write(Base.to_json_string(ls))
