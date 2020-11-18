3884d4a314464ee2bd9e215f0d6e0382d1270e94
poix-code <1996@holbertonschool.com>
2020-11-18 00:47:15 +0000

    Correcting issues

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Lists all privileges of a specific users on a server.
+-- Lists all privileges of a specific
+-- users on a server.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
