138478e007bee96fcfcd3d2d42ab8cc5d5ca4ccf
poix-code <1996@holbertonschool.com>
2020-12-13 15:40:24 +0000

    Correcting pep8 rules

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' \
+ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
