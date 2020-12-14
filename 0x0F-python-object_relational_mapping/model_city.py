5a8290055e7f51e850cf59d03c9ee80fbe13591c
poix-code <1996@holbertonschool.com>
2020-12-14 02:45:05 +0000

    uwu

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class definition 'City'"""
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+class City(Base):
+    """Definition of a City"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
