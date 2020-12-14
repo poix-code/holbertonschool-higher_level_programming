09bd2aa500d2489b24fe8d57fa1bad6e76f8438b
poix-code <1996@holbertonschool.com>
2020-12-14 02:47:15 +0000

    Correcting based on pep8

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
+
 class City(Base):
     """Definition of a City"""
     __tablename__ = 'cities'
