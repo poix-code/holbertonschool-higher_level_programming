33c0ea00bd7b4138d97f72af26457ae7360c9115
poix-code <1996@holbertonschool.com>
2020-12-14 02:07:57 +0000

    Added task #10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Print the name of the state passsed as an argument"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    data = session.query(State).filter(State.name == sys.argv[4]).one_or_none()
+    if data is None:
+        print("Not found")
+    else:
+        print(data.id)
+    session.close()
