1de4f5d6582192669c923264d87f97fe7beb9588
poix-code <1996@holbertonschool.com>
2021-01-11 18:43:41 +0000

    Adding conditions

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@ from urllib import request
 from sys import argv
 
 
-url = argv[1]
-with request.urlopen(url) as response:
-    print(response.headers['X-Request-Id'])
+if __name__ == '__main__':
+    url = argv[1]
+    with request.urlopen(url) as response:
+        print(response.getheader('X-Request-Id'))
