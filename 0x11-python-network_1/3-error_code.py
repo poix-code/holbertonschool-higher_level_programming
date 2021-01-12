c28de07910dd8f2f5efdf235d3eb961bdec460cb
poix-code <1996@holbertonschool.com>
2021-01-12 02:03:49 +0000

    getcode

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         with request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except error.HTTPError as e:
-        print("Error code:", e.code())
+        print("Error code:", e.getcode())
