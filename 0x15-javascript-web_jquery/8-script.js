dbabc9ca929d2bdf1c74f836cd4cb4602cea6abc
poix-code <1996@holbertonschool.com>
2021-02-10 22:06:54 -0500

    append

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+    for (let i = 0; i < data.results.length; i++) { $('<li>' + data.results[i].title + '</li>').appendTo('#list_movies'); }
+  });
+});
